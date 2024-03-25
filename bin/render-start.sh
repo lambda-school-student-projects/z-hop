e4e9c22ccbb4ac3b14fd2432e87932a0b7714c59
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2024-03-25 17:35:41 +0000

    updates render-start

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Uncomment the line depending on the framework you are deploying
 
 # Sinatra
-bundle exec rackup
+#bundle exec rackup
 
 # Ruby on Rails
-# bundle exec puma -C config/puma.rb
+bundle exec puma -C config/puma.rb
